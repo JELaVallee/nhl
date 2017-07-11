84b3340d41d9f3acf019d0506e3befb3c1e66ad6
david weitzman <david@chorus.ai>
2017-07-11 17:52:52 +0300

    fix datepicker toggle

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { MdSelectChange, MdIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
+import { MdSelectChange } from '@angular/material';
 import { PageHeaderService, SEASON_PLACEHOLDER, STAT_PLACEHOLDER } from '../services/page-header/page-header';
 import { SeasonService } from '../services/season/season.service';
 import { ThemeService } from '../services/theme.service';
@@ -16,27 +15,12 @@ export class HeaderComponent {
 
   @Output() toggleSidenav = new EventEmitter<void>();
   selectedOption: string;
-  datepickerIcon: string;
 
 
-  constructor(public pageHeaderService: PageHeaderService, public themeService: ThemeService, private seasonService: SeasonService, private mdIconRegistry: MdIconRegistry, private sanitizer: DomSanitizer) {
-    mdIconRegistry.addSvgIcon('datepicker-toggle-light', sanitizer.bypassSecurityTrustResourceUrl('https://s3.amazonaws.com/dweitz21/nhl/datepicker-toggle-light.svg'));
-    mdIconRegistry.addSvgIcon('datepicker-toggle-dark', sanitizer.bypassSecurityTrustResourceUrl('https://s3.amazonaws.com/dweitz21/nhl/datepicker-toggle-dark.svg'));
-  }
+  constructor(public pageHeaderService: PageHeaderService, public themeService: ThemeService, private seasonService: SeasonService) {}
 
   ngOnInit() {
     this.selectedOption = this.pageHeaderService.options[0] ? this.pageHeaderService.options[0].name : '';
-
-    this.themeService.theme$.subscribe(theme => {
-      switch (theme.type) {
-        case 'light':
-          this.datepickerIcon = 'datepicker-toggle-light';
-          break;
-        case 'dark':
-          this.datepickerIcon = 'datepicker-toggle-dark';
-          break;
-      }
-    });
   }
 
   selectOption(event: MdSelectChange) {
